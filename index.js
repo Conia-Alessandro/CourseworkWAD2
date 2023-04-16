f84be2e5e79cdbec63e31bec5f7076a5e2360227
Conia-Alessandro <ACONIA200@calonian.ac.uk>
2023-04-16 17:39:26 +0100

    added bootstrap and mustache

--- index.js
+++ index.js
@@ -5,11 +5,18 @@ app.use(bodyParser.urlencoded({extended: false}));
 const port = process.env.port || 3001; //the enviroment port
 //TODO nedb requirement
 const path = require('path'); //path dependency, __dirname
+const node_modules_dir = path.join(__dirname,'node_modules');
+const pub = path.join(__dirname,'public'); // public dir
 
-//const pub = path.join(__dirname,'public'); // public dir
+//link to bootstrap
+app.use('/css', express.static(path.join(node_modules_dir,'bootstrap','dist','css'))); 
 
 const fs = require('fs'); // File system dependency
-//TODO mustache or handlebars requirement
+//Mustache dependency
+const mustache = require('mustache-express');
+app.engine('mustache', mustache());
+app.set('view engine', 'mustache');
+
 const router = require('./routes/cwRoutes'); //router for the Routes
 //direct / routes
 app.use('/',router);
